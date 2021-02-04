d5f4ac60515162089654c93ab2b4f43b45c4ec21
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2021-02-03 21:32:14 -0500

    Fixing deployment issue

--- src/main/java/com/myschool/myschoolapp/MyschoolAppApplication.java
+++ src/main/java/com/myschool/myschoolapp/MyschoolAppApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyschoolAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyschoolAppApplication.class, args);
+try{
+	SpringApplication.run(MyschoolAppApplication.class, args);
+}catch(Exception E){
+	E.printStackTrace();
+}
+
 	}
 
 }
